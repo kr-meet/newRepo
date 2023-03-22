c3d9f789d3560e39ee2f1982c3bf3d575c7b2278
kr-meet <123370512+kr-meet@users.noreply.github.com>
2023-03-22 11:07:53 +0530

    Update Counter.t.sol

--- Counter.t.sol
+++ Counter.t.sol
@@ -10,7 +10,7 @@ contract CounterTest is Test {
     ERC20 erc20;
     address meet = address(0x1);
     address manav = address(0x2);
-    address adr;
+    address adr = address(0x3);
 
     function setUp() public {
         erc20 = new ERC20("JARVIS", "JAR");
@@ -32,35 +32,69 @@ contract CounterTest is Test {
         assertEq(100, erc20.totalSupply());
     }
 
-    function testGetSender() public {
-        adr = erc20.getSender();
-        // emit log_address(msg.sender);
-        emit log_address(adr);
-    }
-
-    
-
     function testTransfer() public {
         vm.startPrank(meet);
         bool success = erc20.transfer(manav, 10);
         assertEq(success, true);
-        // vm.endPrank();
-    }
-
-    function testBalanceOf() public {
-        uint256 val = erc20.balanceOf(msg.sender);
-        // emit log_uint(val);
-        assertEq(val, 100);   
+        vm.stopPrank();
     }
 
     function testApprove() public {
 
-        bool success = erc20.approve(manav, uint256(10));
+        erc20.mint(meet, 10);
+
+        vm.startPrank(meet);
+
+        bool success = erc20.approve(manav, 11);
         assertEq(success, true);
+
+        uint256 val = erc20.allowance(meet, manav);
+        emit log_uint(val);
+        assertEq(val, 11);
+
+        erc20.increaseAllowance(manav, 1);
+
+        val = erc20.allowance(meet, manav);
+        emit log_uint(val);
+        assertEq(val, 12);
+
+        erc20.decreaseAllowance(manav, 1);
+
+        val = erc20.allowance(meet, manav);
+        emit log_uint(val);
+        assertEq(val, 11);
+        
+        erc20.transfer(adr, 2);
+        val = erc20.balanceOf(adr);
+        assertEq(val,2);
+
+        vm.stopPrank();  
+        vm.startPrank(manav);
+
+        erc20.transferFrom(meet, adr, 2);
+        val = erc20.balanceOf(adr);
+        assertEq(val,4);
+        emit log_uint(val);
+
+        val = erc20.allowance(meet, manav);
+        emit log_uint(val);
+        assertEq(val, 9);
     }
 
-    function testTranferFrom() public {
-        bool success = erc20.transferFrom(meet, manav, 1);
-        assertEq(success, true);
+    // function testTranferFrom() public {
+    //     bool success = erc20.transferFrom(meet, manav, 1);
+    //     assertEq(success, true);
+    // }
+
+    function testMint() public {
+
+        erc20.mint(meet, 10);
+        uint256 val = erc20.balanceOf(meet);
+        assertEq(val,10);
+        emit log_uint(val);
+        erc20.transfer(meet, manav, 1);
+        val = erc20.balanceOf(manav);
+        assertEq(val,1);
+        emit log_uint(val);
     }
 }
